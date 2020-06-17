404ff666f67c9bb9a79903a713bfe052e0942b03
emma <emma.wishart@live.com.au>
2020-06-17 18:59:40 +0200

    working with data binding and directives for the product list modules

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,5 +5,37 @@ import {Component} from '@angular/core';
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
-    
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage:boolean = false;
+    products: any[] = [
+        {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2019",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "assets/images/leaf_rake.png"
+          },
+          {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+          },
+    ];
+
+    //note that TS does not require the word function for functions
+    toggleImage():void{
+        //using the not on the right hand side means that this will always switch between the two - toggling back and forth
+        this.showImage = !this.showImage;
+    }
+
 }
