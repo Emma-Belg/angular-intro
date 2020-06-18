d83561f49d987c96f8a4d16df375d885075b12ad
emma <emma.wishart@live.com.au>
2020-06-18 10:49:19 +0200

    module 6 done. Learnt about piping and two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  //this is for the directives, components and pipes we write ourselves
   declarations: [
     AppComponent,
     ProductListComponent
   ],
-  imports: [BrowserModule],
+  //this is for stuff(directives, components and pipes) from angular itself or external sources
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
