5ab4630e476d6b1b5c57968aa1e8600ce7edbf52
emma <emma.wishart@live.com.au>
2020-06-19 16:00:09 +0200

    navigation and routing basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template:`
-  <div><h1>{{pageTitle}}</h1>
-    <div>
-      <pm-products></pm-products>
-    </div>
+  template:
+  //Note the syntax for the router link:
+  //we assign the router link directive to an array defned within quotes
+  //the first element of the array is a string, so therefore also in quotes
+  `
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+      <router-outlet></router-outlet>
   </div>
   `
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import {HttpClientModule} from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component'
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   //this is for the directives, components and pipes we write ourselves
@@ -15,13 +18,21 @@ import { ProductDetailComponent } from './products/product-detail.component'
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   //this is for stuff(directives, components and pipes) from angular itself or external sources
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
