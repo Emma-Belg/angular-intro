58f4b4beffbc9e05d1a79c5e2527c34a5b52a7a4
emma <emma.wishart@live.com.au>
2020-06-19 17:21:29 +0200

    guarding routes and other routing techniques

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
 import {IProduct} from './product';
 
 @Component({
@@ -9,9 +11,29 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  //we are setting ActivatedRoute as a dependency and injecting it into this component by defining it as a parameter in this constructor
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    //b/c the param is provided as a string we have the plus "+" at the beginning
+    //the + is a js shortcut to convert the param string an int
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product =   {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
